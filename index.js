0655771bfbec324592bce000ece6bc843e73b2a8
Sam Ralston <sam.ralston46@gmail.com>
2022-03-14 15:54:12 +1100

    fixed mongo issues

--- index.js
+++ index.js
@@ -3,8 +3,10 @@
 var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
-var mongodb = require("mongodb");
-var ObjectID = mongodb.ObjectID;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+
+
 
 var CONTACTS_COLLECTION = "contacts";
 
@@ -12,26 +14,27 @@ var app = express();
 app.use(express.static(__dirname + "/src"));
 app.use(bodyParser.json());
 
-// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-var db;
 
-// Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
+async function main(){
 
-  // Save database object from the callback for reuse.
-  db = database;
-  console.log("Database connection ready");
+  // get initial data
+  const client = new MongoClient(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+  client.connect(err => {
+    // const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+  });
 
   // Initialize the app.
   var server = app.listen(process.env.PORT || 5000, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
   });
-});
+
+}
+
+main().catch(console.error);
+
 
 // ENDPOINTS
 
